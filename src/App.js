b19dea4020daf0d32b78d1e9c5112e09a1e33d04
RomuloLeocadio <103466625+RomuloLeocadio@users.noreply.github.com>
2023-03-27 20:08:59 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import {useState, useEffect} from 'react';
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+
+function App() {
+
+  //Objeto produto
+  const produto ={
+    codigo : 0,
+    nome : '',
+    marca : ''
+  }
+
+  //UseState
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+
+  //UseEffect
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+  //Obter os dados do formulario
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto, [e.target.name]:e.target.value});
+  }
+
+  //Cadastrar produto
+  const cadastrar = () => {
+    fetch('http://localhost:8080/cadastrar', {
+      method: 'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+      'Content-type':'application/json',
+      'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+
+      if(retorno_convertido.mensagem !== undefined){
+        alert(retorno_convertido.mensagem);
+      }
+      else{
+        setProdutos([...produtos, retorno_convertido]);
+        alert('Produto cadastrado com sucesso!');
+      }
+      
+    })
+  }
+  
+
+  // Selecionar produto
+  const selecionarProduto = (indice) => {
+      setObjProduto(produtos[indice]);
+      setBtnCadastrar(false);
+  }
+  
+  //Retorno
+  return (
+    <div>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar}/>
+      <Tabela vetor={produtos} selecionar={selecionarProduto}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Formulario.js
@@ -0,0 +1,23 @@
+function Formulario({botao, eventoTeclado, cadastrar}){
+    return(
+        <form>
+            <input type='text' onChange={eventoTeclado} name='nome' placeholder='Nome' className='form-control' />
+            <input type='text' onChange={eventoTeclado} name='marca' placeholder='Marca' className='form-control' />
+
+            {
+                botao
+                ?
+                <input type='button' value='Cadastrar' onClick={cadastrar} className='btn btn-primary' />
+                :
+                <div>
+                     <input type='button' value='Alterar' className='btn btn-warning' />
+                     <input type='button' value='Cancelar' className='btn btn-secondary' />
+                </div>
+            }
+            
+
+        </form>
+    )
+}
+
+export default Formulario;
